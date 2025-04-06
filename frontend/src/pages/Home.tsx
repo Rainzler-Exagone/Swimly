453a47a39512ad46a350afaf80e667db41c80586
Rainzler Exagone <yasser458891@gmail.com>
2025-04-06 22:55:23 +0100

    some changes in styling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,17 @@
 import { CardWithForm } from "@/components/Athletecard";
+import Navbar from "@/components/Navbar";
 
 function Home() {
-    return (<CardWithForm/>)
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <main className="pt-24 px-4">
+          <CardWithForm />
+        </main>
+      </div>
+    </>
+  );
 }
-
 
 export default Home;
